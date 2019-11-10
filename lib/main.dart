825483b8632d87908013e23821a7a60049cb2256
Godsend <bobbyjoseph108@gmail.com>
2019-11-10 11:57:58 +0100

    'flutter_app'

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+
+  //run app method
+  runApp(
+    Center(
+      child: Text(
+      "Hello Flutter",
+      textDirection: TextDirection.ltr,
+     ),
+    )
+  );
+}
