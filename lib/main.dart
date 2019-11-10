d3a6e9e7a4dc35934e5e98caea3ce3e674c3f0a6
Godsend <bobbyjoseph108@gmail.com>
2019-11-10 12:35:36 +0100

    'first_ui'

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,31 @@ void main() {
 
   //run app method
   runApp(
-    Center(
-      child: Text(
-      "Hello Flutter",
-      textDirection: TextDirection.ltr,
-     ),
+
+    MaterialApp(
+      title: "My Flutter App", // app name
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First Flutter App"),),
+
+        //body of the app
+        body: Material( // material design body for the app body
+
+          color: Colors.lightBlue, //color for the app
+
+          child: Center( // widget to center text
+
+            child: Text( // child widget to display text
+
+              "Hello Flutter", // text that displays
+
+              textDirection: TextDirection.ltr, // text direction
+
+              style: TextStyle(color: Colors.white, fontSize: 40.0), // styling
+
+            ),
+          ),
+        ),
+      )
     )
   );
 }
